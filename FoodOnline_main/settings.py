566090bd40a9ffe169447c346fb15faf85a1e0c6
Manoja-Reddy7 <128035700+Manoja-Reddy7@users.noreply.github.com>
2024-02-16 22:32:17 +0530

    Customer and vendor registration

--- FoodOnline_main/settings.py
+++ FoodOnline_main/settings.py
@@ -103,6 +103,5 @@ from django.contrib.messages import constants as messages
 
 MESSAGE_TAGS = {
     messages.ERROR: "danger",
-    messages.ERROR: "success",
-   
-}
+    messages.SUCCESS: "success",
+   }
--- accounts/views.py
+++ accounts/views.py
@@ -2,21 +2,42 @@ from django.shortcuts import render,redirect
 from vendor.forms import VendorForm
 from .forms import UserForm
 from .models import User,UserProfile
-from django.contrib import messages
+from django.contrib import messages,auth
+from .utils import detectUser
+from django.contrib.auth.decorators import login_required,user_passes_test
+from django.core.exceptions import PermissionDenied
 
-# Create your views here.
+# Restrict the Vendor from customer page
+
+def check_role_vendor(user):
+    if user.role==1:
+        return True
+    else:
+        raise PermissionDenied
+
+# Restrict the customer  from Vendor page
+
+def check_role_customer(user):
+    if user.role==2:
+        return True
+    else:
+        raise PermissionDenied
+
 
 def registerUser(request):
+     if request.user.is_authenticated:
+        messages.warning(request,'You are already logged in!')
+        return redirect('myAccount')
     
-    '''In the context of web forms, 
-    POST requests are used when submitting data to the server, such as when a user submits a form.'''
-    if request.method=='POST':
+        '''In the context of web forms, 
+        POST requests are used when submitting data to the server, such as when a user submits a form.'''
+     elif request.method=='POST':
         # request.POST prints the contents of the request.POST dictionary to the console.
         # form = UserForm(request.POST),it creates an instance of the UserForm using the data from request.POST
         # This condition checks if the submitted form data is valid according to the rules defined in the UserForm class.
-        print(request.POST)
-        form = UserForm(request.POST)
-        if form.is_valid():
+         print(request.POST)
+         form = UserForm(request.POST)
+         if form.is_valid():
             # the form is ready to save, this is not yet save.
             # After we submit the form the data with fields is stored in the cleaned_data dictionary.
             password = form.cleaned_data['password']
@@ -24,19 +45,19 @@ def registerUser(request):
             user.role = User.CUSTOMER
             user.set_password(password)
             user.save()
-            messages.error(request,'Your account has been registered successfully!')
+            messages.success(request,'Your account has been registered successfully!')
             return redirect('registerUser')
-        else:
+         else:
             print(form.errors)
             print("Invalid Form.")
-    else:
+     else:
         # When user first visit and enter the webpage url the form will display on the webpage i.e GET request.
         # calling the Userform class 
         form = UserForm()
-    context = {
+        context = {
          'form':form,
              }
-    return render(request, 'accounts/registerUser.html',context)
+        return render(request, 'accounts/registerUser.html',context)
 
 def registerVendor(request):
     userform = UserForm()
@@ -45,7 +66,11 @@ def registerVendor(request):
             'form'  : userform,
             'v_form': v_form,
            }
-    if request.method=='POST':
+    if request.user.is_authenticated:
+        messages.warning(request,'You are already logged in!')
+        return redirect('myAccount')
+    
+    elif request.method=='POST':
         # store the data and create user
         form = UserForm(request.POST)
         v_form = VendorForm(request.POST,request.FILES)
@@ -78,8 +103,59 @@ def registerVendor(request):
            }'''
     return render(request,'accounts/registerVendor.html',context)
 
+def login(request):
+    if request.user.is_authenticated:
+        messages.warning(request,'You are already logged in!')
+        return redirect('myAccount')
+        
+        
+    elif request.method =="POST":
+        email = request.POST.get('email')
+        password = request.POST.get('password')
+        user = auth.authenticate(email=email,password=password)
+        
+        if user is not None and user.is_active:
+            auth.login(request,user)
+            messages.success(request, 'You are logged in.')
+            return redirect('myAccount')
+        else:
+            messages.error(request,'Invalid log in credentials')
+            return redirect('login')
+            
+    return render(request,'accounts/login.html')
+
+def logout(request):
+    auth.logout(request)
+    messages.info(request,'You are logged out.')
+    return redirect('login')
+
+
+# When you logged into the account then only you have access to the myaccount dashboard.
+
+@login_required(login_url='login')
+def myAccount(request):
+    user = request.user
+    print(user)
+    redirecturl = detectUser(user)
+    return redirect(redirecturl)
+
+@login_required(login_url='login')   
+@user_passes_test(check_role_customer)
+def custDashboard(request):
+    return render (request,'accounts/custDashboard.html')
+
+
+@login_required(login_url='login')
+@user_passes_test(check_role_vendor)
+def vendorDashboard(request):
+    return render (request,'accounts/vendorDashboard.html')
+
+
+
     
     
        
     
     
+    
+        
