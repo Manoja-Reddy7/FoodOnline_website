9e7e53a446d13f038a478a01661cd933466c41a5
Manoja-Reddy7 <128035700+Manoja-Reddy7@users.noreply.github.com>
2024-06-10 20:45:28 +0530

    Orders and order detail functionalities - customer/vendor

--- orders/admin.py
+++ orders/admin.py
@@ -11,7 +11,7 @@ class OrderedFoodInline(admin.TabularInline):
      extra = 0 # To remove extra blanks.
      
 class OrderAdmin(admin.ModelAdmin):
-    list_display = ('order_number', 'name','phone','email','total','payment_method','status','is_ordered')
+    list_display = ('order_number', 'name','phone','email','total','payment_method','status','is_ordered','order_placed_to')
     inlines = [OrderedFoodInline]
 
 # Register your models here.
